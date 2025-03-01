9559a546dfb9bd8da65c2582d00fd5da727f9757
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-01 12:19:13 -0500

    feat: implement pureba-redux in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CounterComponent } from './components/counter/counter.component';
+import { PruebaReduxComponent } from './components/prueba-redux/prueba-redux.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CounterComponent],
+  imports: [CounterComponent, PruebaReduxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
