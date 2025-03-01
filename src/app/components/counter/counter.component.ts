8b30dacebe912a4d3b1343c1380aea3f81435c3c
Juan Pablo Giraldo <jpgthf@gmail.com>
2025-03-01 12:18:23 -0500

    change: implementation to redux

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../../store/items.action';
 
 @Component({
   selector: 'app-counter',
@@ -8,24 +10,29 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   title: string = 'Contador Redux';
-  counter: number;
+  counter: number = 0;
 
-  constructor(){
-    this.counter = 0;
+  constructor(private store: Store<{counter: number}>){
+    store.select('counter').subscribe( counter => {
+      this.counter = counter;
+    });
   }
 
   increment(){
-    this.counter++;
+    // this.counter++;
+    this.store.dispatch(increment());
     console.log("Incrementando...");
   }
 
   decrement(){
-    this.counter--;
+    // this.counter--;
+    this.store.dispatch(decrement());
     console.log("Decrementando...");
   }
 
   reset(){
-    this.counter = 0;
+    // this.counter = 0;
+    this.store.dispatch(reset());
     console.log("Reseteando...");
   }
 }
